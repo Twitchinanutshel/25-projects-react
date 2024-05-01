670df838ce9b92971aa229d6307cb2b847522b6b
Twitchinanutshel <breakf117@gmail.com>
2024-05-01 16:23:00 +1000

    third

---
+++ src/components/NavButtons.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+
+const NavButtons = () => {
+  const linkClass = ({ isActive }) => (
+    isActive
+      ? 'text-2xl p-4 group underline underline-offset-8 decoration-orange-500'
+      : 'text-2xl p-4 group hover:underline underline-offset-8'
+  );
+
+  return (
+    <div className='absolute top-0 right-0 mr-4'>
+      <div className='flex space-x-2 text-white text-xl font-josefin'>
+        <NavLink
+          to='/'
+          className={linkClass}
+        >Home
+        </NavLink>
+
+      </div>
+    </div>
+  )
+}
+
+export default NavButtons
